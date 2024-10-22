3cbe584c3613c58698f914a276be87cb21326827
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-22 21:29:40 +0530

    js3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-var 
+var number=[1,2,3];
+number.push(4);
+console.log(number);//[ 1, 2, 3, 4 ]
+var number1=[1,2,3,4];
+number1.pop();
+console.log(number1);//[ 1, 2, 3 ]
+var number2=[2,3,4];
+number2.unshift(1);
+console.log(number2);//[ 1, 2, 3, 4 ]
+var number3=[1,2,3,4];
+number3.shift();
+console.log(number3);//[ 2, 3, 4 ]
+var number4=[1,2,3];
+ number4=number4.join("");
+console.log(number4);
+
+
+
