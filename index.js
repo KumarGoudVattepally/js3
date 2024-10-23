d7d133d355718dbfefcfb708ebec97166d670c80
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-23 08:38:53 +0530

    js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var number=[1,2,3];
+var number = [1, 2, 3];
 number.push(4);
-console.log(number);//[ 1, 2, 3, 4 ]
-var number1=[1,2,3,4];
+console.log(number); //[ 1, 2, 3, 4 ]
+var number1 = [1, 2, 3, 4];
 number1.pop();
-console.log(number1);//[ 1, 2, 3 ]
-var number2=[2,3,4];
+console.log(number1); //[ 1, 2, 3 ]
+var number2 = [2, 3, 4];
 number2.unshift(1);
-console.log(number2);//[ 1, 2, 3, 4 ]
-var number3=[1,2,3,4];
+console.log(number2); //[ 1, 2, 3, 4 ]
+var number3 = [1, 2, 3, 4];
 number3.shift();
-console.log(number3);//[ 2, 3, 4 ]
-var num4=[1,2,3];
- num5=num4.toString();
+console.log(number3); //[ 2, 3, 4 ]
+var num4 = [1, 2, 3];
+num5 = num4.toString();
 console.log(num5);
-
-
-
-
